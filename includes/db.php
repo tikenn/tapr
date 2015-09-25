65965dcc7d4b00be2b2c6d77973f651624c12bb3
tikenn <tikenn@users.noreply.github.com>
2015-09-24 19:12:49 -0500

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -1,6 +1,6 @@
 <?php
 $db_host = "localhost";
-$db_user = "atradb";
-$db_pass = "wNSTGp8jAZB1mSYr6NIP0Nzk3vJjm6Zr";
-$db_db = "atradb";
+$db_user = "dummydb";
+$db_pass = "your_pass";
+$db_db = "dummydb";
 ?>
